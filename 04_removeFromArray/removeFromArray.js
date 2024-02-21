fce1ff023447bf46794132a3ad4c1cf389486caf
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-02-21 14:22:47 -0600

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, ...args) {
+  let newArray = [];
+  input.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  })
+  return newArray;
 };
 
 // Do not edit below this line
