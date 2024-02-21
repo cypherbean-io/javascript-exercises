23864c9f023ea0a8c0143284700a41d7b9e4f0b7
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-02-21 11:04:57 -0600

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, iterations) {
+  let myString = "";
+  if (iterations < 0) return "ERROR";
+  for (i = 0; i < iterations; i++) {
+    myString = myString.concat(word);
+  }
+  return myString;
 };
 
 // Do not edit below this line
