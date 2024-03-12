06796d8993f94bee2dae909ac84e6feb06496088
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-03-12 10:16:42 -0500

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let b = 1;
+  let array= [];
+  while (b <= a) {
+    array.push(b);
+    b++;
+  }
+  return array.reduce((total, num) => total * num, 1);
 };
 
 // Do not edit below this line
