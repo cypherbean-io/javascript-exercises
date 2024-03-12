55dff8fd0f56be2d7ad8ef191ccf520f9958c928
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-03-12 14:09:30 -0500

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const myDate = new Date();
+  const currentYear = myDate.getFullYear();
+  for (let i = 0; i < people.length; i++) {
+    if (!people[i].yearOfDeath) {
+      people[i].yearOfDeath = currentYear;
+    }
+    people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
+  }
+  let answer = people.sort((a, b) => b.age - a.age);
+  return answer[0];
 };
 
 // Do not edit below this line
