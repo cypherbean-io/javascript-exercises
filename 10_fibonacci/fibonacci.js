5fa90b35c2046eca69ffb494e84b7bd1937b1a9c
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-03-12 11:43:35 -0500

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+  let target = Number(input);
+  if (target < 0) return "OOPS";
+  let a = 0;
+  let b = 1;
+  let next;
+  let fibonacciNumbers = []
+  for (let i = 0; i <= target; i++) {
+    fibonacciNumbers.push(a);
+    next = a + b;
+    a = b;
+    b = next;
+  };
+  return fibonacciNumbers[target];
 };
 
 // Do not edit below this line
